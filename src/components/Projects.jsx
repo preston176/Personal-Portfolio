abcb9c3ddf1da6dd3ad8edfaf2b54d8adcfc6425
preston176 <prestonnyamweya@gmail.com>
2023-08-04 15:27:16 +0300

    projects text

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Projects = () => {
   return (
-    <div>
-      projects
+    <div className='px-7 md:px-10 my-8' id='projects'>
+      <h1 className='text-3xl mt-5 text-primary font-semibold'>Projects</h1>
+      <p className='text-white my-2 md:w-2/3 leading-[2]'>I have worked on a number of projects ... checkout some of my featured projects</p>
+      {/* project items */}
     </div>
   );
 };
