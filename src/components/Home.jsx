8553411e2f0d97b9368bb0aba13f2a4f9b7770bd
preston176 <prestonnyamweya@gmail.com>
2023-08-04 14:19:41 +0300

    buttons styled

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,12 @@ const Home = () => {
                 <div className="text-white">
                 <h5 className='text-3xl mt-12'>Hello there, I'm</h5>
                 <h1 className='font-semibold text-3xl md:text-5xl my-4'>Preston Mayieka</h1>
-                <p>I am a ...</p>
+                <p className='md:w-96'>I am a ...</p>
+            {/* buttons section */}
+            <div className="">
+                <button className='btn bg-primary py-2 px-4 text-white rounded hover:bg-white hover:text-primary transition-all duration-500'>About me</button>
+                <button className='btn outline px-5 py-1.5 rounded border-none text-white ml-5'>Download CV</button>
+            </div>
                 </div>
             </div>
         </div>
