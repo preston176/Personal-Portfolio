329681bce5c6ad2c39205f87c63490b164402394
preston176 <prestonnyamweya@gmail.com>
2023-08-04 14:41:30 +0300

    about section styled + placeholder text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      About
+    <div className='px-7 md:px-10 sm:mt-24' id='about'>
+      <h1 className='text-3xl mt-5 text-primary font-semibold'>About me</h1>
+      <p className='text-white my-2 md:w-2/3 leading-[2]'>I am ... a</p>
+   {/* techstack */}
+   <div className="md:flex items-center my-7">
+    <p className='text-primary text-8xl font-bold'>2+</p>
+    <p className='text-white text-2xl md: ml-5'>Years of experience in working with web development technologies</p>
+   </div>
     </div>
   );
 };
