f699f26fe70d7d33a709aba77eb7424772395823
preston176 <prestonnyamweya@gmail.com>
2023-08-04 14:15:47 +0300

    styled home + placeholder

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            Home
+        <div className='px-7 md:px-10 my-14 md:h-screen bg-[#1a1a29]'>
+            <div className="">
+                {/* texts section */}
+                <div className="text-white">
+                <h5 className='text-3xl mt-12'>Hello there, I'm</h5>
+                <h1 className='font-semibold text-3xl md:text-5xl my-4'>Preston Mayieka</h1>
+                <p>I am a ...</p>
+                </div>
+            </div>
         </div>
     );
 };
