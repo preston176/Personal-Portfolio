69a9012679f8d56fcf6d39b35b9a67eccb1dcc28
preston176 <prestonnyamweya@gmail.com>
2023-08-04 14:10:41 +0300

    conditional styling working in nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,7 +22,7 @@ const Header = () => {
         </div>
 
         {/* md: mobile icons */}
-        <div onClick={()=> {setOpen(!open)}} className='w-7 h-7 right-8 top-6 cursor-pointer md:hidden text-white absolute'>
+        <div onClick={() => setOpen(!open)} className='w-7 h-7 right-8 top-6 cursor-pointer md:hidden text-white absolute'>
           {
             /* show only if open... */
             open ? <XMarkIcon /> : <Bars3BottomRightIcon />
@@ -30,7 +30,7 @@ const Header = () => {
         </div>
 
         {/* navbar */}
-        <ul className={`md:flex md: items-center md:pd-0 absolute md:static md:z-auto z-10 left-0 w-full md:w-auto mt-4 md:pl-0 pl-9 bg-slate-400 sm:bg-transparent transition-all duration-500 ease-in-out`}>
+        <ul className={`md:flex md:items-center md:pd-0 absolute md:static md:z-auto z-10 left-0 w-full md:w-auto mt-4 md:pl-0 pl-9 bg-slate-400 sm:bg-transparent transition-all duration-500 ease-in-out ${open ? "top-12" : "top-[-490px]"}`}>
           {links.map((item) =>
             <li key={crypto.randomUUID()}
               className='md:ml-8 md:my-0 my:7 font-semibold'>
