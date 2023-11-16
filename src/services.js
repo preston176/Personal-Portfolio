cb6c0c934a8f1bfa1078032f15be814f645a9d7b
preston176 <prestonnyamweya@gmail.com>
2023-11-16 11:05:42 +0300

    Update services.js with new content writing service

--- src/services.js
+++ src/services.js
@@ -4,7 +4,7 @@ const services = [
     { id: 3, name: 'MOBILE DEVELOPMENT', description: 'I am capable of creating amazing mobile applications though React Native', image: '/assets/icons/android.png' },
     { id: 4, name: 'VERSION CONTROL', description: 'I can use version control systems well, and Git & Mecurial are my go-to tool.', image: '/assets/icons/git-repo.png' },
     { id: 5, name: 'NPM AND NODEJS', description: 'I have core understanding of NPM. I can also develop general purpose applications with NodeJS', image: '/assets/icons/javascript.png' },
-    { id: 6, name: 'WEB SCRAPING', description: 'I can collect content and data from the internet then manipulate and analyze as needed.', image: '/assets/icons/web-scraping.png' },
+    { id: 6, name: 'CONTENT WRITING', description: 'I have a strong understanding of the content writing process and I can write any content from scratch. I have experience with writing articles, blogs, and essays.', image: '/assets/icons/web-scraping.png' },
 ]
 
 export default services;
