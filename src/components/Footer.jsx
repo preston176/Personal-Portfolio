0d1f2d0dd6a07a0a3a4488391ff1e79c0797de45
preston176 <prestonnyamweya@gmail.com>
2024-02-06 22:19:14 +0300

    Update copyright year in Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { SocialIcon } from 'react-social-icons';
 
 const Footer = () => {
+  // Get the current year
+  const date = new Date();
+  const year = date.getFullYear();
+
+
   return (
     <footer className="relative bg-blueGray-200 pt-8 pb-6">
       <div className="container mx-auto px-4">
@@ -65,8 +70,8 @@ const Footer = () => {
         <div className="flex flex-wrap items-center md:justify-between justify-center">
           <div className="w-full md:w-4/12 px-4 mx-auto text-center">
             <div className="text-sm text-slate-200 font-semibold py-1">
-              Copyright © <span id="get-current-year">2023</span>
-              <a href="https://www.creative-tim.com/product/notus-js" className="text-blueGray-500 hover:text-gray-800" target="_blank" rel="noopener noreferrer">
+              Copyright © <span id="get-current-year">{year}</span>
+              <a href="https://codewithpres.hashnode.dev" className="text-blueGray-500 hover:text-gray-800" target="_blank" rel="noopener noreferrer">
                 {/* Notus JS by <a href="https://www.creative-tim.com?ref=njs-profile" className="text-blueGray-500 hover:text-blueGray-800">Creative Tim</a>. */}
               </a>
             </div>
