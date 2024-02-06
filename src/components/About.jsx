5e888892fdf6626d2bb1cc6333f3ca156aea24ad
preston176 <prestonnyamweya@gmail.com>
2024-02-06 22:30:48 +0300

    Update About component styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -23,7 +23,7 @@ const About = () => {
   return (
     <div className='px-7 md:px-10 sm:mt-24' id='about'>
       <h1 className='text-3xl mt-5 text-primary font-semibold'>About me</h1>
-      <p className='text-white my-2 md:w-2/3 leading-[2]' >I enjoy solving technical problems, researching and developing new technologies, designing software applications for different platforms. I enjoy meeting people and working with them in a team environment. I also enjoy interacting with clients and customers. My strong customer service skills allow me to do this well. I am a quick learner with a fun, outgoing personality. In addition, I excel in my ability to work under pressure and handle stressful situations very well.</p>
+      <p className='text-white my-2 md:w-11/12 leading-[2]' >I enjoy solving technical problems, researching and developing new technologies, designing software applications for different platforms. I enjoy meeting people and working with them in a team environment. I also enjoy interacting with clients and customers. My strong customer service skills allow me to do this well. I am a quick learner with a fun, outgoing personality. In addition, I excel in my ability to work under pressure and handle stressful situations very well.</p>
       {/* techstack */}
       <div className="md:flex items-center my-7">
         <p className='text-primary text-8xl font-bold'>{yearsOfExperience}+</p>
@@ -31,7 +31,7 @@ const About = () => {
 
       </div>
       {/* skills card */}
-      <div className='flex flex-col md:flex-row '>
+      <div className='flex flex-col md:flex-row md:flex-wrap sm:flex-wrap '>
         {
           skills.map((item, i) => {
             return (
