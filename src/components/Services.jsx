558b2ef81272b29ba594662e9380d51df466e17c
preston176 <prestonnyamweya@gmail.com>
2024-02-09 23:57:33 +0300

    Add react-helmet and alt text to service images

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -12,7 +12,7 @@ const Services = () => {
                     services.map((serv, i) => {
                         return (
                             <div key={i} className='service-card flex flex-col justify-center items-center p-6 text-center text-white'>
-                                <img src={serv.image} alt="" />
+                                <img src={serv.image} alt={serv.altText} />
                                 <h5 className='text-primary font-semibold text-3xl mt-14'>{serv.title}</h5>
                                 <p className='text-primary font-semibold text-lg'>
                                     {serv.description}
