1d149869c02b299f59fdf0427f6aac28d4c394be
preston176 <prestonnyamweya@gmail.com>
2024-03-19 13:31:01 +0300

    removed stats section

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -43,7 +43,7 @@ const LandingPage = () => {
             <About />
             <Projects />
             <Blogs />
-            <Stats />
+            {/* <Stats /> */}
             <Contact />
             <Footer />
             {/* Back to top Icon */}
