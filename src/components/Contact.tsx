210c06704515da7d9e0b3ba22850c99cf40fa1db
Preston M <prestonnyamweya@gmail.com>
2024-11-13 12:48:46 +0300

    feat: update Contact component with improved form button styling and layout adjustments

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -56,45 +56,50 @@ const Contact = () => {
   };
 
 
-    return (
-        <>
-            <div className='px-7 md:px-10 my-10' id='contact'>
-                <div className="flex flex-col md:flex-row">
-                    {/* texts and icons */}
-                    <div className="md:w-1/2">
-                        <h1 className='text-3xl mt-5 text-primary font-semibold'>Connect with me: </h1>
-                        <p className='text-white my-2 md:w-2/3 leading-[2]'>
-                            {/* social icons */}
-                            <div className="flex mx-2 hover:cursor-pointer">
-                                {/* <SocialIcons /> */}
-                            </div>
-                        </p>
-                    </div>
-                    {/* contact form */}
-                    <div className="md:w-1/2">
-                        <p className='mt-16 text-white text-2xl mb-6'>Feel free to reach me out ... </p>
-                        <ToastContainer />
+  return (
+    <>
+      <div className='px-7 md:px-10 my-10' id='contact'>
+        <div className="flex flex-col md:flex-row">
+          {/* texts and icons */}
+          <div className="md:w-1/2">
+            <h1 className='text-3xl mt-5 text-primary font-semibold'>Connect with me: </h1>
+            <p className='text-white my-2 md:w-2/3 leading-[2]'>
+              {/* social icons */}
+              <div className="flex mx-2 hover:cursor-pointer">
+                {/* <SocialIcons /> */}
+              </div>
+            </p>
+          </div>
+          {/* contact form */}
+          <div className="md:w-1/2">
+            <p className='mt-16 text-white text-2xl mb-6'>Feel free to reach me out ... </p>
+            <ToastContainer />
 
-                        <form onSubmit={handleFormSubmit} className='text-white' >
-                            <input type="text" name='name' id='name' placeholder='Your Name:' required className='bg-[#F5F5F5] bg-opacity-10 py-3 px-3 md:w-3/4 w-full rounded my-3' />
-                            <input type="email" name='email' id='email' placeholder='Your email:' required className='bg-[#F5F5F5] bg-opacity-10 py-3 px-3 md:w-3/4 w-full rounded my-3' />
-                            <ValidationError
-                                prefix="Email"
-                                field="email"
-                                errors={state.errors} />
-                            <textarea required name="message" id="message" cols={30} rows={5} placeholder='Message: ' className='bg-[#F5F5F5] bg-opacity-10 py-3 px-3 md:w-3/4 w-full rounded my-3'></textarea>
-                            <ValidationError
-                                prefix="Message"
-                                field="message"
-                                errors={state.errors}
-                            />
-                            <button disabled={state.submitting} className='btn bg-primary py-2 px-12 text-white hover:bg-white hover:text-primary block transition-all duration-500 rounded'>Send</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+            <form onSubmit={handleFormSubmit} className='text-white' >
+              <input type="text" name='name' id='name' placeholder='Your Name:' required className='bg-[#F5F5F5] bg-opacity-10 py-3 px-3 md:w-3/4 w-full rounded my-3' />
+              <input type="email" name='email' id='email' placeholder='Your email:' required className='bg-[#F5F5F5] bg-opacity-10 py-3 px-3 md:w-3/4 w-full rounded my-3' />
+              <ValidationError
+                prefix="Email"
+                field="email"
+                errors={state.errors} />
+              <textarea required name="message" id="message" cols={30} rows={5} placeholder='Message: ' className='bg-[#F5F5F5] bg-opacity-10 py-3 px-3 md:w-3/4 w-full rounded my-3'></textarea>
+              <ValidationError
+                prefix="Message"
+                field="message"
+                errors={state.errors}
+              />
+              <button
+                disabled={state.submitting}
+                className="btn bg-white py-2 px-12 text-black hover:bg-black hover:text-white block transition-all duration-500 rounded"
+              >
+                Send
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Contact;
