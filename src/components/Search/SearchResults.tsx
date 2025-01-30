91e5a0e84418f52c41ac6cbb708aeed450db95aa
preston176 <prestonnyamweya@gmail.com>
2025-01-30 23:40:38 +0300

    refactor: remove unused context and effects from Search and SearchResults components

--- src/components/Search/SearchResults.tsx
+++ src/components/Search/SearchResults.tsx
@@ -1,6 +1,5 @@
-import { useContext } from "react";
 import { Link } from "../Header/Links";
-import { GlobalContext } from "../../context/GlobalContext";
+
 
 interface Props {
     results: Link[];
@@ -8,7 +7,6 @@ interface Props {
 
 
 const SearchResults: React.FC<Props> = ({ results }) => {
-    const { setSearch } = useContext(GlobalContext);
 
     return (
         <div className="w-full bg-white shadow-lg rounded-sm mt-4 max-h-[300px] overflow-y-scroll">
@@ -19,7 +17,7 @@ const SearchResults: React.FC<Props> = ({ results }) => {
                             key={index}
                             className="p-4 hover:bg-gray-100 flex items-center space-x-4 transition-colors duration-200"
                         >
-                            <a href={`/#${result.link}`} onClick={() => setSearch(false)} className="flex items-center">
+                            <a href={`/#${result.link}`} className="flex items-center">
                                 <div className="flex-shrink-0">
                                     <span className="inline-block w-8 h-8 bg-[#1a1a29] rounded-full text-center place-content-center font-bold text-white">
                                         {result.name?.charAt(0).toUpperCase()}
