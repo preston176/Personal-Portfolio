3d9b9a03c462ddff1b725b857347cbbd905feff4
preston176 <prestonnyamweya@gmail.com>
2025-03-30 23:26:02 +0300

    feat: add Projects section to showcase portfolio and update Header navigation

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import { useState, useEffect } from "react";
 
 export const Header = () => {
   const navLinks = [
@@ -11,47 +10,18 @@ export const Header = () => {
     { href: "/#contact", label: "Contact" },
   ];
 
-  const [activeSection, setActiveSection] = useState<string | null>(null);
-
-  useEffect(() => {
-    const sections = navLinks
-      .filter(({ href }) => href.startsWith("/#"))
-      .map(({ href }) => document.querySelector(href.replace("/", "")));
-
-    const observer = new IntersectionObserver(
-      (entries) => {
-        const visibleSection = entries.find((entry) => entry.isIntersecting);
-        if (visibleSection) {
-          setActiveSection(`#${visibleSection.target.id}`);
-        }
-      },
-      { threshold: 0.5 } // Adjust for when to consider section "active"
-    );
-
-    sections.forEach((section) => {
-      if (section) observer.observe(section);
-    });
-
-    return () => {
-      sections.forEach((section) => {
-        if (section) observer.unobserve(section);
-      });
-    };
-  }, []);
-
   return (
-    <div className="flex justify-center items-center fixed top-3 left-0 right-0 z-50">
+    <div className="flex justify-center items-center relative top-3">
       <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur-sm">
-        {navLinks.map(({ href, label }) => {
-          const isActive = href === "/" ? activeSection === null : href === activeSection;
+        {navLinks.map(({ href, label }, index) => {
+          const isContact = index === navLinks.length - 1;
 
           return (
             <Link
               key={href}
               href={href}
-              className={`nav-item px-4 py-2 rounded-full transition-colors ${
-                isActive ? "bg-white text-gray-900 hover:bg-white/70" : "text-white hover:text-gray-300"
-              }`}
+              className={`nav-item px-4 py-2 rounded-full transition-colors ${isContact ? "bg-green-500 text-black hover:bg-green-400 hover:text-black" : "text-gray-300 hover:text-gray-400"
+                }`}
             >
               {label}
             </Link>
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,19 +1,23 @@
-import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
+import nexusAILandingPage from "@/assets/images/nexus-ai-landing-page.png";
 import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+import Link from "next/link";
+import CheckCircleIcon from "@/assets/icons/check-circle.svg"
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg"
+import Image from "next/image";
 
 const portfolioProjects = [
   {
     company: "Solo Project",
-    year: "2022",
+    year: "2025",
     title: "NexusAI",
     results: [
       { title: "Learnt how to do Payment integration for SaaS apps." },
       { title: "Used Langchain, Pinecone and Groq for embeddings." },
-      { title: "Designed a clea, distraction-free interface." },
+      { title: "Designed a clean, distraction-free interface." },
     ],
     link: "https://youtu.be/4k7IdSLxh6w",
-    image: darkSaasLandingPage,
+    image: nexusAILandingPage,
   },
   {
     company: "Innovative Co",
@@ -42,5 +46,46 @@ const portfolioProjects = [
 ];
 
 export const ProjectsSection = () => {
-  return <div>Projects Section</div>;
+  return <section className="pb-16" id="projects">
+    <div className="container">
+      <div className="flex justify-center">
+        <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-green-500 to-green-200 text-transparent bg-clip-text text-center">Real-world Results</p>
+      </div>
+      <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">Featured Projects</h2>
+      <p className="text-center md:text-lg text-white/60 mt-4 max-w-md mx-auto">See how I ...</p>
+      <div className="flex flex-col mt-10 gap-20 md:mt-20">
+        {
+          portfolioProjects.map((project, index) => (
+            <div key={index} className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 md:pt-12 md:px-10 after:pointer-events-none">
+
+              <div className="bg-gradient-to-r from-green-500 to-green-200 inline-flex gap-2 font-bold uppercase tracking-widest text-sm bg-clip-text text-transparent">
+                <span>{project.company}</span>
+                <span>&bull;</span>
+                <span>{project.year}</span>
+              </div>
+
+              <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl">{project.title}</h3>
+              <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
+              <Image src={project.image} alt={project.title} className="mt-8 md:-mb-0 rounded-md" quality={100} />
+              <ul className="flex flex-col gap-4 mt-4">
+                {project.results.map((result, index) => (
+                  <li key={index} className="flex gap-2 text-sm md:text-base text-white/50">
+                    <CheckCircleIcon className="size-5 md:size-6" />
+                    <span>{result.title}</span>
+                  </li>
+                ))}
+              </ul>
+              <Link href={project.link}>
+                <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8 mb-6 hover:underline ">
+                  <span>View Live Site</span>
+                  <ArrowUpRightIcon className="size-4" />
+                </button>
+              </Link>
+
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  </section>;
 };
