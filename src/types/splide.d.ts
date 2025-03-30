e86cda17eeba181b2702cc406607df71f794bc4a
preston176 <prestonnyamweya@gmail.com>
2025-03-30 13:20:35 +0300

    init next

--- src/types/splide.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "@splidejs/react-splide" {
-    import { ComponentType } from "react";
-    import { SplideProps, SplideSlideProps } from "@splidejs/react-splide/dist/types";
-  
-    export const Splide: ComponentType<SplideProps>;
-    export const SplideSlide: ComponentType<SplideSlideProps>;
-  }
-  
