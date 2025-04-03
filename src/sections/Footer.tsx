359a4a4ab8108e1b77f21501b6505c802489465d
preston176 <prestonnyamweya@gmail.com>
2025-04-03 19:01:57 +0300

    feat: implement Footer section with dynamic year and day, add navigation links

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,24 @@
+import Link from "next/link";
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg"
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 export const Footer = () => {
-  return <div>Footer Section</div>;
+
+
+  return <footer>
+
+    <div className="container">
+      <div className="border-t border-white/15 py-6 text-sm flex flex-col items-center">
+        <div className="text-white/90">&copy;{new Date().getFullYear()}. Have a nice {days[new Date().getUTCDay()]} {":)"}</div>
+        <div>
+          <nav>
+            <Link className="inline-flex items-center gap-1.5" href={"https://codewithpres.hashnode.dev/"}><span>Hashnode</span> <ArrowUpRightIcon /></Link>
+            <Link href={"#"}></Link>
+            <Link href={"#"}></Link>
+            <Link href={"#"}></Link>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </footer>;
 };
