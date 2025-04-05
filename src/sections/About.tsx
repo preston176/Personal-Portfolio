1f729ca4453107b1f1599e70befde8dcb80e140f
preston176 <prestonnyamweya@gmail.com>
2025-04-05 11:11:19 +0300

    feat: about section

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,11 +1,13 @@
 import Card from "@/components/Card";
 import SectionHeader from "@/components/SectionHeader";
 import bookImage from "@/assets/images/book-cover.png";
+import ArticleImage from "@/assets/images/ArticleImage.png"
 import Image from "next/image";
 import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaNodeJs, FaGitAlt, FaGithub, FaNpm } from "react-icons/fa";
 import { SiNextdotjs, SiTailwindcss, SiTypescript, SiJest, SiPostgresql, SiFigma, SiPrisma } from "react-icons/si";
 import { CardHeader } from "@/components/CardHeader";
 import { SkillItems } from "@/components/SkillItems";
+import Link from "next/link";
 
 const skillSet = [
   { title: "JavaScript", icon: FaJs },
@@ -41,20 +43,23 @@ export const AboutSection = () => {
       <div className="container">
         <SectionHeader
           eyebrow="About Me"
-          title="A glimpse into my world"
+          title="Crafting Code with Purpose"
           description="Learn more about who I am and what I do"
         />
         <div className="mt-20 flex flex-col gap-8">
           <div className="grid grid-cols-1 gap-8 md:grid-cols-5 lg:grid-cols-3">
-            <Card className="h-[320px] md:col-span-2 col-span-3 lg:col-span-1">
+          <Link href={"https://codewithpres.hashnode.dev/"} target="_blank" className="hover:scale-105 ">
+            <Card className="h-[320px] md:col-span-2 col-span-3 lg:col-span-1 hover:bg-slate-600 transition duration-150 ease-in">
               <CardHeader
                 title="My Articles"
                 description="Dive into topics I write about—tech, tutorials, and more."
               />
-              <div className="w-40 mx-auto">
-                <Image src={bookImage} alt="Article thumbnail" />
-              </div>
+             
+                <div className="w-full my-7  mx-auto">
+                  <Image className="rounded-md" src={ArticleImage} alt="Article thumbnail" />
+                </div>
             </Card>
+              </Link>
             <Card className="h-[320px] p-0 col-span-3 lg:col-span-2">
               <CardHeader title="Tech Stack" description="Explore the technologies and tools I use for my craft" className="px-6 pt-6" />
               <SkillItems skillSet={skillSet} className="mt-6" />
