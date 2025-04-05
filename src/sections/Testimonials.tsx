c48def376012a76e90f3eec677fa4a8c22f5e21f
preston176 <prestonnyamweya@gmail.com>
2025-04-05 08:25:26 +0300

    feat: update Testimonials section by commenting out unused testimonials and adding a new entry

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,5 +1,5 @@
 import memojiAvatar1 from "@/assets/images/memoji-avatar-1.png";
-import memojiAvatar2 from "@/assets/images/memoji-avatar-2.png";
+// import memojiAvatar2 from "@/assets/images/memoji-avatar-2.png";
 import memojiAvatar3 from "@/assets/images/memoji-avatar-3.png";
 import memojiAvatar4 from "@/assets/images/memoji-avatar-4.png";
 import memojiAvatar5 from "@/assets/images/memoji-avatar-5.png";
@@ -14,12 +14,12 @@ const testimonials = [
     text: "Working with Preston was a pleasure. He is one of the few Software Engineers who can fix PC-Related issues.",
     avatar: memojiAvatar1,
   },
-  {
-    name: "Olivia Green",
-    position: "Head of Design @ GreenLeaf",
-    text: "Working with Alex was a pleasure. His expertise in frontend development brought our designs to life in a way we never imagined. The website has exceeded our expectations.",
-    avatar: memojiAvatar2,
-  },
+  // {
+  //   name: "",
+  //   position: "Head of Design @ GreenLeaf",
+  //   text: "Working with Alex was a pleasure. His expertise in frontend development brought our designs to life in a way we never imagined. The website has exceeded our expectations.",
+  //   avatar: memojiAvatar5,
+  // },
   {
     name: "Emmanuel Shikuku",
     position: "CEO @ Calatech",
@@ -27,16 +27,10 @@ const testimonials = [
     avatar: memojiAvatar3,
   },
   {
-    name: "",
-    position: "Product Manager @ GlobalTech",
-    text: "Alex is a true frontend wizard. He took our complex product and transformed it into an intuitive and engaging user interface. We're already seeing positive feedback from our customers.",
+    name: "Benard Oloo",
+    position: "ICT Director @ KPLC",
+    text: "He is one of a kind. During his industrial attachment he adapted quickly and participated in  ",
     avatar: memojiAvatar4,
-  },
-  {
-    name: "Michael Brown",
-    position: "Director of IT @ MegaCorp",
-    text: "Alex's work on our website has been nothing short of exceptional. He's a talented developer who is also a great communicator. We highly recommend him.",
-    avatar: memojiAvatar5,
   },
 ];
 
