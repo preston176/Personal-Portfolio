00f5c8b7a50535caa93534af68d1e86f0cb923f3
preston176 <prestonnyamweya@gmail.com>
2025-04-15 19:35:41 +0300

    fix: add "use client" directive to Footer component

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Link from "next/link";
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
 
@@ -26,7 +28,7 @@ export const Footer = () => {
     <footer className="relative overflow-x-clip">
       {/* Background decoration, stays behind */}
       <div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/10 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10"></div>
-      
+
       <div className="container relative">
         <div className="border-t border-white/15 py-6 text-sm flex flex-col md:flex-row md:justify-between gap-8 items-center">
           <div className="text-white/90">
