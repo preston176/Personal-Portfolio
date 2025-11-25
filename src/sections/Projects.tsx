fa6b495a3dacaa6fa61b0d4fd01404e7564a15f6
preston176 <prestonnyamweya@gmail.com>
2025-11-25 10:02:02 +0300

    refactor: restructure ProjectsSection to improve layout and remove AOS animation

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -3,24 +3,25 @@
 import SectionHeader from "@/components/SectionHeader";
 import { portfolioProjects } from "../../lib/constants";
 import ProjectCard from "@/components/ProjectCard";
-import { useEffect } from "react";
-import AOS from "aos";
-import 'aos/dist/aos.css';
 
 export const ProjectsSection = () => {
-  useEffect(() => {
-    AOS.init({
-      duration: 800, // animation duration in ms
-    });
-  }, []);
-
   return (
     <section className="pb-16 lg:py-24" id="projects">
       <div className="container">
         <SectionHeader eyebrow=" Real-world Results" title="Featured Projects" description="Explore impactful projects that delivered real-world value." />
-        <div className="flex flex-col mt-10 gap-20 md:mt-20">
+        <div className="mt-10 md:mt-20">
           {portfolioProjects.map((project, index) => (
-            <ProjectCard key={index} index={index} project={project} />
+            <div
+              key={index}
+              className="sticky"
+              style={{
+                top: `${40 + index * 40}px`,
+                marginBottom: index < portfolioProjects.length - 1 ? '120px' : '0',
+                zIndex: index + 1,
+              }}
+            >
+              <ProjectCard index={index} project={project} />
+            </div>
           ))}
         </div>
       </div>
